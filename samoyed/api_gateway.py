43d8045c737b6e32f35b15b33b768ddeca3a882d
Seonghyeon Kim <self@seonghyeon.dev>
2020-09-01 11:42:37 +0900

    feat: Improve api gateway handler with exception

--- samoyed/api_gateway.py
+++ samoyed/api_gateway.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass, field
 from functools import wraps
-from typing import Literal, Optional, Union, Dict, List, Callable
+from typing import Any, Callable, Dict, List, Optional, TypeVar, Union
+
+from typing_extensions import Literal
+
+VT = TypeVar("VT")
+Mapping = Dict[str, VT]
+MultiValueMapping = Dict[str, List[VT]]
 
 
 @dataclass(frozen=True)
@@ -12,15 +18,40 @@ class APIGatewayProxyEvent:
     resource: str  # Resource path
     path: str  # Path parameter
     http_method: Literal["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]  # Incoming request's method name
-    headers: Dict[str, str]  # {String containing incoming request headers}
-    multi_value_headers: Dict[str, List[str]]  # {List of strings containing incoming request headers}
-    query_string_parameters: Dict[str, str]  # {query string parameters }
-    multi_value_query_string_parameters: Optional[Dict[str, List[str]]]  # {List of query string parameters}
-    path_parameters: Optional[Dict[str, str]]  # {path parameters}
-    stage_variables: Optional[Dict[str, str]]  # {Applicable stage variables}
-    request_context: Dict  # {Request context, including authorizer-returned key-value pairs} TODO: Objectify
+    headers: Mapping[Union[bool, int, float, str]]  # {String containing incoming request headers}
+    multi_value_headers: MultiValueMapping[
+        Union[bool, int, float, str]
+    ]  # {List of strings containing incoming request headers}
+    query_string_parameters: Mapping[str]  # {query string parameters }
+    multi_value_query_string_parameters: Optional[MultiValueMapping[str]]  # {List of query string parameters}
+    path_parameters: Optional[Mapping[str]]  # {path parameters}
+    stage_variables: Optional[Mapping[str]]  # {Applicable stage variables}
+    request_context: Mapping[Any]  # {Request context, including authorizer-returned key-value pairs} TODO: Objectify
     body: Optional[str]  # A JSON string of the request payload.
     is_base64_encoded: bool  # A boolean flag to indicate if the applicable request payload is Base64-encode
+
+
+class APIGatewayProxyBaseException(Exception):
+    status_code: int = 500
+    body: str = ""
+    headers: Dict[str, Union[bool, int, float, str]] = {}
+    multi_value_headers: Dict[str, List[Union[bool, int, float, str]]] = {}
+    is_base64_encoded: bool = False
+
+    def __init__(
+        self,
+        status_code: int = None,
+        body: str = None,
+        *,
+        is_base64_encoded: bool = None,
+        headers: Mapping[str] = None,
+        multi_value_headers: MultiValueMapping[Union[bool, int, float, str]] = None
+    ):
+        self.status_code = self.status_code if status_code is None else status_code
+        self.body = self.body if body is None else body
+        self.is_base64_encoded = self.is_base64_encoded if is_base64_encoded is None else is_base64_encoded
+        self.headers = self.headers if headers is None else headers
+        self.multi_value_headers = self.multi_value_headers if multi_value_headers is None else multi_value_headers
 
 
 @dataclass
@@ -31,9 +62,19 @@ class APIGatewayProxyResult:
 
     status_code: int
     body: str
-    headers: Dict[str, Union[bool, int, float, str]] = field(default={})
-    multi_value_headers: Dict[str, List[Union[bool, int, float, str]]] = field(default={})
+    headers: Mapping[Union[bool, int, float, str]] = field(default_factory=dict)
+    multi_value_headers: MultiValueMapping[Union[bool, int, float, str]] = field(default_factory=dict)
     is_base64_encoded: bool = False
+
+    @staticmethod
+    def from_exception(exc: APIGatewayProxyBaseException) -> "APIGatewayProxyResult":
+        return APIGatewayProxyResult(
+            status_code=exc.status_code,
+            body=exc.body,
+            headers=exc.headers,
+            multi_value_headers=exc.multi_value_headers,
+            is_base64_encoded=exc.is_base64_encoded,
+        )
 
 
 RestApiHandler = Callable[[APIGatewayProxyEvent, dict], APIGatewayProxyResult]
@@ -54,9 +95,12 @@ def rest_api_handler(fn: RestApiHandler) -> Callable[[dict, dict], APIGatewayPro
             stage_variables=event["stageVariables"],
             request_context=event["requestContext"],
             body=event["body"],
-            is_base64_encoded=event["isBase64Encoded"]
+            is_base64_encoded=event["isBase64Encoded"],
         )
 
-        return fn(rest_api_event, context)
+        try:
+            return fn(rest_api_event, context)
+        except APIGatewayProxyBaseException as e:
+            return APIGatewayProxyResult.from_exception(e)
 
     return wrapper
